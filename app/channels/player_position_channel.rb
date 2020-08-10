3fcd0e89afd6d8c8b494f53b08c8993e5975e07b
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-11 03:27:11 +0900

    ユーザの接続状態の変更をブロードキャストする

--- app/channels/player_position_channel.rb
+++ app/channels/player_position_channel.rb
@@ -4,11 +4,26 @@ class PlayerPositionChannel < ApplicationCable::Channel
   ALLOWED_PARAMETERS = %w[id x y z].freeze
 
   def subscribed
+    @id = params['id']
+
     stream_from 'player_position'
+
+    broadcast_to_group('subscribed', { id: @id })
   end
 
   def receive(data)
     data.reject! { |key| ALLOWED_PARAMETERS.exclude? key }
-    ActionCable.server.broadcast('player_position', data)
+
+    broadcast_to_group('update', data.merge({ id: @id }))
+  end
+
+  def unsubscribed
+    broadcast_to_group('unsubscribed', { id: @id })
+  end
+
+  private
+
+  def broadcast_to_group(type, payload)
+    ActionCable.server.broadcast('player_position', { type: type, payload: payload })
   end
 end
