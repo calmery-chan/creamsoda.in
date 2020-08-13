ff787f2e54709027a78cdc13fc4eaf1b05126a26
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-14 04:07:45 +0900

    管理者向け API を追加できるようにする

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Admin::ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |t|
+      ActiveSupport::SecurityUtils.secure_compare(t, ENV['ADMIN_AUTHORIZATION_TOKEN'] || '')
+    end
+  end
+end
---
+++ app/controllers/admin/serial_codes_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Admin::SerialCodesController < Admin::ApplicationController
+  def index
+    render status: 200
+  end
+end
