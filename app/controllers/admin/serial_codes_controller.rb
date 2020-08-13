17413b287e3f613344f3a24afbc29fb3e08c889b
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-14 04:36:21 +0900

    kaminari を追加、ページを指定して SerialCode を取得できるようにする

--- app/controllers/admin/serial_codes_controller.rb
+++ app/controllers/admin/serial_codes_controller.rb
@@ -2,6 +2,6 @@
 
 class Admin::SerialCodesController < Admin::ApplicationController
   def index
-    @serial_codes = SerialCode.all
+    @serial_codes = SerialCode.all.page(params[:page]).per(10)
   end
 end
