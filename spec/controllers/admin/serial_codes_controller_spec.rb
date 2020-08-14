f11cdefd06a2918649ec1bec9767c29a289a81b7
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-14 13:52:03 +0900

    SerialCode のテストを修正する

--- spec/controllers/admin/serial_codes_controller_spec.rb
+++ spec/controllers/admin/serial_codes_controller_spec.rb
@@ -5,23 +5,35 @@ require 'rails_helper'
 # rubocop:disable Metrics/BlockLength
 RSpec.describe Admin::SerialCodesController, type: :controller do
   describe '#index' do
-    let!(:serial_code_1) { SerialCode.create! }
-    let!(:serial_code_2) { SerialCode.create! }
-    let!(:serial_code_3) { SerialCode.create! }
-    let!(:serial_code_4) { SerialCode.create! }
+    before do
+      15.times do
+        SerialCode.create!
+      end
+    end
 
     it 'ID 昇順で取得できる' do
       get :index, format: :json
 
-      expect(response).to have_http_status :ok
+      data = JSON.parse(response.body)['data']
+      ids = data.map { |s| s['id'] }
+
+      expect(ids).to match ids.sort
+    end
+
+    it '1 ページ目の SerialCode を取得できる' do
+      get :index, format: :json
 
       data = JSON.parse(response.body)['data']
 
-      expect(data.size).to eq 4
-      expect(data[0]['serial_code']).to eq serial_code_1.serial_code
-      expect(data[1]['serial_code']).to eq serial_code_2.serial_code
-      expect(data[2]['serial_code']).to eq serial_code_3.serial_code
-      expect(data[3]['serial_code']).to eq serial_code_4.serial_code
+      expect(data.size).to eq 10
+    end
+
+    it '2 ページ目の SerialCode を取得できる' do
+      get :index, format: :json, params: { page: 2 }
+
+      data = JSON.parse(response.body)['data']
+
+      expect(data.size).to eq 5
     end
   end
 
@@ -58,49 +70,20 @@ RSpec.describe Admin::SerialCodesController, type: :controller do
   end
 
   describe '#update' do
-    describe 'ID に対応した SerialCode が存在するとき' do
-      let!(:serial_code_state_created) { SerialCode.create!(state: SerialCode.states[:created]) }
-      let!(:serial_code_state_allowed) { SerialCode.create!(state: SerialCode.states[:allowed]) }
-      let!(:serial_code_state_denied) { SerialCode.create!(state: SerialCode.states[:denied]) }
+    context 'ID に対応した SerialCode が存在するとき' do
+      let!(:serial_code_state) { SerialCode.create!(state: SerialCode.states[:created]) }
 
-      context 'SerialCode の state を created から' do
-        it 'allowed に変更することができる' do
-          put :update, format: :json, params: { id: serial_code_state_created.id, state: :allowed }
-          expect(response).to have_http_status :ok
-          expect(JSON.parse(response.body)['data']['state']).to eq 'allowed'
-        end
+      it '更新できる' do
+        put :update, format: :json, params: { id: serial_code_state.id, state: :allowed }
 
-        it 'denied に変更することができる' do
-          put :update, format: :json, params: { id: serial_code_state_created.id, state: :denied }
-          expect(response).to have_http_status :ok
-          expect(JSON.parse(response.body)['data']['state']).to eq 'denied'
-        end
+        expect(response).to have_http_status :ok
       end
 
-      context 'SerialCode の state を allowed から' do
-        it 'created に変更することはできない' do
-          put :update, format: :json, params: { id: serial_code_state_allowed.id, state: :created }
-          expect(response).to have_http_status :bad_request
-        end
+      it 'ActiveRecord::RecordNotSaved が呼ばれたとき、400 を返す' do
+        allow_any_instance_of(SerialCode).to receive(:update!).and_raise ActiveRecord::RecordNotSaved
+        put :update, format: :json, params: { id: serial_code_state.id }
 
-        it 'denied に変更することができる' do
-          put :update, format: :json, params: { id: serial_code_state_allowed.id, state: :denied }
-          expect(response).to have_http_status :ok
-          expect(JSON.parse(response.body)['data']['state']).to eq 'denied'
-        end
-      end
-
-      context 'SerialCode の state を denied から' do
-        it 'created に変更することはできない' do
-          put :update, format: :json, params: { id: serial_code_state_denied.id, state: :created }
-          expect(response).to have_http_status :bad_request
-        end
-
-        it 'allowed に変更することができる' do
-          put :update, format: :json, params: { id: serial_code_state_denied.id, state: :allowed }
-          expect(response).to have_http_status :ok
-          expect(JSON.parse(response.body)['data']['state']).to eq 'allowed'
-        end
+        expect(response).to have_http_status :bad_request
       end
     end
 
@@ -118,13 +101,14 @@ RSpec.describe Admin::SerialCodesController, type: :controller do
     let!(:serial_code_state_allowed) { SerialCode.create!(state: SerialCode.states[:allowed]) }
 
     context 'ID に対応した SerialCode が存在するとき' do
-      it 'SerialCode の state が created であれば削除できる' do
+      it '削除できる' do
         post :destroy, format: :json, params: { id: serial_code_state_created.id }
 
         expect(response).to have_http_status :ok
       end
 
-      it 'SerialCode の state が created 以外であれば削除できない' do
+      it 'ActiveRecord::RecordNotDestroyed が呼ばれたとき、400 を返す' do
+        allow_any_instance_of(SerialCode).to receive(:destroy!).and_raise ActiveRecord::RecordNotDestroyed
         post :destroy, format: :json, params: { id: serial_code_state_allowed.id }
 
         expect(response).to have_http_status :bad_request
