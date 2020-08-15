ea7a4662f9ab574520a9b9d9e966c084236e068d
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-16 04:16:01 +0900

    RuboCop に従ってソースコードを修正する

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,10 +9,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "metaneno.vercel.app"
+    origins 'localhost:3000', 'metaneno.vercel.app'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
