5babaeb245c739f2df7e1eb9b53217119b08702f
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-17 19:20:43 +0900

    Administrator モデルを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_174123) do
+ActiveRecord::Schema.define(version: 2020_08_17_100518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "administrators", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_administrators_on_name"
+  end
 
   create_table "serial_codes", force: :cascade do |t|
     t.string "serial_code", null: false
