96ff50c193b6b47885bfe57010c4c49df15bef8d
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-26 19:35:24 +0900

    Contentful の Content Management API を使えるようにする

--- config/initializers/contentful.rb
+++ config/initializers/contentful.rb
@@ -3,5 +3,6 @@
 ContentfulModel.configure do |config|
   config.access_token = ENV['CONTENTFUL_ACCESS_TOKEN']
   config.environment = ENV['CONTENTFUL_ENVIRONMENT']
+  config.management_token = ENV['CONTENTFUL_MANAGEMENT_TOKEN'] # Personal Access Token
   config.space = ENV['CONTENTFUL_SPACE']
 end
