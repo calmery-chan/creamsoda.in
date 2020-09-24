ecdd8b3abaf5b801251cbe7b677f09f0a8163ab6
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-25 00:37:43 +0900

    SerialCodesController を修正、テストを追加する

--- app/controllers/serial_codes_controller.rb
+++ app/controllers/serial_codes_controller.rb
@@ -4,7 +4,10 @@ class SerialCodesController < ApplicationController
   def show
     serial_code = SerialCode.find_by!(serial_code: params['serial_code'])
 
-    return if serial_code.allowed?
+    if serial_code.allowed?
+      status 200
+      return
+    end
 
     status 401 if serial_code.created?
     status 403 if serial_code.denied?
