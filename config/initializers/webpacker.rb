ffcc0a397b01c731d1c7464cd55222920b036e5b
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-29 01:07:22 +0900

    RuboCop に従って修正する

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,2 +1,4 @@
-Webpacker::Compiler.env["NEXT_PUBLIC_RECAPTCHA_SITE_KEY"] = ENV['NEXT_PUBLIC_RECAPTCHA_SITE_KEY']
-Webpacker::Compiler.env["SENTRY_DSN"] = ENV['REACT_SENTRY_DSN']
+# frozen_string_literal: true
+
+Webpacker::Compiler.env['NEXT_PUBLIC_RECAPTCHA_SITE_KEY'] = ENV['NEXT_PUBLIC_RECAPTCHA_SITE_KEY']
+Webpacker::Compiler.env['SENTRY_DSN'] = ENV['SENTRY_DSN_REACT']
