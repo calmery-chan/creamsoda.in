99fc374a678ce57e7c5488d82c1d668a83ffc637
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-12-03 00:10:07 +0900

    開発環境では Cloudinary に画像をアップロードしない

--- app/uploaders/temporary_image_uploader.rb
+++ app/uploaders/temporary_image_uploader.rb
@@ -3,12 +3,28 @@
 class TemporaryImageUploader < CarrierWave::Uploader::Base
   TEMPORARY_IMAGE_FOLDER_PATH = "temporary/images"
 
-  def store!(image)
-    @asset = Cloudinary::Uploader.upload image, :folder => TEMPORARY_IMAGE_FOLDER_PATH, :public_id => public_id
-  end
+  if Rails.env.production?
+    def store!(image)
+      @asset = Cloudinary::Uploader.upload image, :folder => TEMPORARY_IMAGE_FOLDER_PATH, :public_id => public_id
+    end
 
-  def url
-    @asset['secure_url']
+    def url
+      @asset['secure_url']
+    end
+  else
+    storage :file
+
+    def asset_host
+      'http://localhost:5000'
+    end
+
+    def filename
+      "#{public_id}.#{file.extension}"
+    end
+
+    def store_dir
+      "uploads/#{TEMPORARY_IMAGE_FOLDER_PATH}"
+    end
   end
 
   private
