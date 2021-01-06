07d7a1fe77ddb137cb1f1a4eef9ef623b02071f2
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-07 01:39:09 +0900

    Jest の設定を jest.config.ts に移動する

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  collectCoverageFrom: ['**/*.(j|t)s'],
+  coverageDirectory: '../coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
