02555881c327820569dd50f4115c7d2eef43b2de
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-08 03:18:47 +0900

    Heroku で動作するように app.listen の第二引数に 0.0.0.0 を渡す

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   cors(app);
   session(app);
 
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 5000, "0.0.0.0");
 }
 
 bootstrap();
