fc547f47f7e9e07140e94f34516f5e239e1b7d79
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-07 23:31:59 +0900

    Sentry の init 時に行っていた SENTRY_DSN の確認処理を削除する

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,19 +1,17 @@
 import Sentry from "@sentry/node";
 
-if (process.env.SENTRY_DSN) {
-  Sentry.init({
-    beforeSend: (event) => {
-      if (process.env.NODE_ENV === "production") {
-        return event;
-      }
+Sentry.init({
+  beforeSend: (event) => {
+    if (process.env.NODE_ENV === "production") {
+      return event;
+    }
 
-      // Production 環境以外ではエラーの内容をそのまま出力する
-      console.error(event);
+    // Production 環境以外ではエラーの内容をそのまま出力する
+    console.error(event);
 
-      return null;
-    },
-    dsn: process.env.SENTRY_DSN,
-  });
-}
+    return null;
+  },
+  dsn: process.env.SENTRY_DSN,
+});
 
 export { Sentry };
