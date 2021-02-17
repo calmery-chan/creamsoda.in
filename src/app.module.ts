207fd8b2c8999abd66ecc79a80a99da045199d47
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-17 10:38:33 +0900

    multiplay.service.ts を追加する

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ChekiModule } from "./cheki/cheki.module";
 import { MultiplayGateway } from "./multiplay.gateway";
+import { MultiplayService } from "./multiplay/multiplay.service";
 import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
   imports: [ConfigModule.forRoot(), AdminModule, ChekiModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, MultiplayGateway],
+  providers: [AppService, PrismaService, MultiplayGateway, MultiplayService],
 })
 export class AppModule {}
